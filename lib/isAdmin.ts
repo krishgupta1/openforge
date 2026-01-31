a2db7766070f4a172fc68ebdce4e037829682307
Krish Gupta <krishgupta0072@gmail.com>
2026-01-31 11:23:44 +0530

    temp

--- lib/isAdmin.ts
+++ lib/isAdmin.ts
@@ -2,7 +2,7 @@ export const ADMINS = [
   "krishgupta0072@gmail.com",
   "sahilmishra03032005@gmail.com"
 ];
-
+  
 export function isAdmin(email?: string | null) {
   return email ? ADMINS.includes(email) : false;
 }
